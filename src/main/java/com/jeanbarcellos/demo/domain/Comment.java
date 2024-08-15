8132b6dd1f6caec72d2d91c9dc8112bfe17199f9
Jean Barcellos <jeanbarcellos@hotmail.com>
2024-08-14 21:21:46 -0300

    demo - Criação das primeiras entities

---
+++ src/main/java/com/jeanbarcellos/demo/domain/Comment.java
@@ -0,0 +1,51 @@
+package com.jeanbarcellos.demo.domain;
+
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Type;
+
+import com.jeanbarcellos.core.domain.IEntity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor(staticName = "of")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Builder
+@Entity
+@Table(name = "comment")
+public class Comment implements IEntity {
+
+    @Id
+    @Type(type = "uuid-char")
+    @Column(name = "id", nullable = false, updatable = false)
+    @EqualsAndHashCode.Include
+    private UUID id;
+
+    @Type(type = "uuid-char")
+    @ManyToOne
+    @JoinColumn(name = "post_id", referencedColumnName = "id", foreignKey = @ForeignKey(name = "comment_post_id_fk"), nullable = false)
+    private Post post;
+
+    @Column(name = "author", nullable = false)
+    private String author;
+
+    @Column(name = "text", nullable = false)
+    private String text;
+
+}
