d337f6db21afafa749d933aca81a14f222761c32
Jean Barcellos <jeanbarcellos@hotmail.com>
2024-08-24 15:56:26 -0300

    core - Implementation
    - Criação de MapperBase

---
+++ src/main/java/com/jeanbarcellos/core/MapperBase.java
@@ -0,0 +1,42 @@
+package com.jeanbarcellos.core;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public abstract class MapperBase {
+
+    private ModelMapper modelMapper;
+
+    protected <S, D> D map(S source, Class<D> destinationType) {
+        return this.getModelMapper()
+                .map(source, destinationType);
+    }
+
+    protected <S, D> List<D> mapList(List<S> source, Class<D> destinationType) {
+        var mp = this.getModelMapper();
+
+        return source
+                .stream()
+                .map(element -> mp.map(element, destinationType))
+                .collect(Collectors.toList());
+    }
+
+    private ModelMapper getModelMapper() {
+        if (this.modelMapper != null) {
+            return modelMapper;
+        }
+
+        var mp = new ModelMapper();
+
+        mp.getConfiguration()
+                .setSkipNullEnabled(true);
+
+        return mp;
+    }
+
+}
